79aedc7a1282697874d478ad9c43f1b73e71ac55
p1402835 <Maganik@users.noreply.github.com>
2016-03-08 14:38:07 +0100

    changement pour une meilleure architecture

---
+++ autodidacte/src/Neuron.c
@@ -0,0 +1,57 @@
+#ifdef __cplusplus
+#error This source file is not C++ but rather C. Please use a C-compiler
+#endif
+
+
+	/*  Neuron.C   -----   LIF7 Auto-Didacte */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "Neuron.h"
+
+
+Neuron * newNeuron(char type, int nbHiddenNeuron)
+{
+
+}
+
+void deleteNeuron(Neuron * neuron)
+{
+
+}
+
+
+
+
+float getValue(Neuron * neuron)
+{
+
+}
+
+void  setValue(Neuron * neuron, float newValue)
+{
+
+}
+
+
+
+
+
+float getWeight(Neuron * neuron, int i)
+{
+
+}
+
+void  setWeight(Neuron * neuron, int i, float newWeight)
+{
+
+}
+
+
+
+void  update(Neuron * neuron)
+{
+
+}
