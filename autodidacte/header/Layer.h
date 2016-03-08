2e0832124fbf78c989f62518f9c507d8088b8edb
p1402835 <p1402835@localhost>
2016-03-08 14:38:07 +0100

    changement pour une meilleure architecture

---
+++ autodidacte/header/Layer.h
@@ -0,0 +1,35 @@
+ #ifndef H_LAYER
+ #define H_LAYER
+ 
+
+  /* Layer.h    -----   LIF7 Auto-Didacte */
+
+
+#include "Neuron.h"
+
+/* Layer structure*/
+
+typedef struct{
+
+	char type;
+	int nbNeurons;
+	Neuron * tabNeurons;
+
+}Layer;
+
+/* Layer functions */
+
+Layer * newLayer(char type , int nbNeurons);
+void deleteLayer(Layer * layer);
+
+char getNbNeurons(const Layer* layer);
+void setNbNeurons(int nbNeurons);
+
+Neuron * getNeuron(const Layer* layer, int i);
+void setNeuron(Layer * layer, int i);
+
+void update(Layer * layer);
+
+
+#endif
+
---
+++ autodidacte/header/Neuron.h
@@ -0,0 +1,36 @@
+ #ifndef H_NEURON
+ #define H_NEURON
+ 
+
+  /* Neuron.h   -----   LIF7 Auto-Didacte */
+
+
+
+/* Neuron structure */
+
+typedef struct
+{
+
+    char type;
+    float value;
+    float * tabWeights;
+
+} Neuron;
+
+
+
+
+/* Neuron functions */
+
+Neuron * newNeuron(char type, int nbHiddenNeuron);
+void deleteNeuron(Neuron * neuron);
+
+float getValue(Neuron * neuron);
+void  setValue(Neuron * neuron, float newValue);
+
+float getWeight(Neuron * neuron, int i);
+void  setWeight(Neuron * neuron, int i, float newWeight);
+
+void  update(Neuron * neuron);
+
+#endif
