4fd2342996490ab7d8f32594decd563159590ba5
p1402835 <Maganik@users.noreply.github.com>
2016-03-08 10:03:08 +0100

    Projet code blocks fichier main et neuron.c/.h

---
+++ autodidacte/src/link/link.h
@@ -0,0 +1,10 @@
+#ifndef LINK_H_INCLUDED
+#define LINK_H_INCLUDED
+#include "../neuron/neuron.h"
+typedef struct Links
+{
+   Neuron * neuron;
+   double weight;
+}; Link
+
+#endif // LINK_H_INCLUDED
