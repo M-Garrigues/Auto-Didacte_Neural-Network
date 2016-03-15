2ac51555dc79947ea048be0fbedb38f8a66cb305
p1402835 <p1402835@localhost>
2016-03-15 21:10:57 +0100

    generation.c et .h avanc?s mais grosse fuite memoire

--- autodidacte/src/Generation.c
+++ autodidacte/src/Generation.c
@@ -3,28 +3,32 @@
 #include "assert.h"
 #include "stdlib.h"
 
-/* 25 sera le nombre d'individus dans une generation */
-Generation * newGenerationRandom()
+
+Generation * newGenerationRandom(int nbSubject)
 {
     int i;
     Generation * pGen = malloc(sizeof(Generation));
     assert(pGen != NULL);
-    pGen->tabGenomes = malloc(sizeof(Genome *)*25);
-    for(i = 0; i<25;i++)
+    pGen->nbGen = 0;
+    pGen->nbSubject = nbSubject;
+    pGen->tabGenomes = malloc(sizeof(Genome *)*nbSubject);
+    for(i = 0; i<nbSubject;i++)
     {
-        setGenome(&pGen, i, newGenomeRandom(8));
+        setGenome(&pGen, i, newGenomeRandom(12));
     }
     return pGen;
 }
-Generation * newGenerationNull()
+Generation * newGenerationNull(int nbSubject)
 {
     int i;
     Generation * pGen = malloc(sizeof(Generation));
     assert(pGen != NULL);
-    pGen->tabGenomes = malloc(sizeof(Genome *)*25);
-    for(i = 0; i<25;i++)
+    pGen->nbGen = 0;
+    pGen->nbSubject = nbSubject;
+    pGen->tabGenomes = malloc(sizeof(Genome *)*nbSubject);
+    for(i = 0; i<nbSubject;i++)
     {
-        setGenome(&pGen, i, newGenomeRandom(8));
+        setGenome(&pGen, i, newGenomeNull(12));
     }
     return pGen;
 }
@@ -58,6 +62,14 @@ void setNbGen(Generation ** gen, int num)
 {
     (*gen)->nbGen = num;
 }
+int getNbSubject(const Generation * gen)
+{
+    return gen->nbSubject;
+}
+void setNbSubject(Generation ** gen, int nb)
+{
+    (*gen)->nbSubject =nb;
+}
 
 void sauverGeneration(const Generation * gen, FILE * f)
 {
@@ -66,4 +78,86 @@ void sauverGeneration(const Generation * gen, FILE * f)
 void chargerGeneration(Generation ** gen, FILE * f)
 {
     
+}
+
+void nextGeneration(Generation ** gen)
+{
+
+    updateFitnessGeneration(gen);
+    /* saveGeneraton */
+    crossoverGeneration(gen);
+    mutationGeneration(gen);
+}
+void updateFitnessGeneration(Generation ** gen)
+{
+    int i;
+
+    for(i = 0; i<getNbSubject(*gen);i++)
+    {
+        
+        updateFitnessGenome(&(*gen)->tabGenomes[i]);
+    }
+}
+void crossoverGeneration(Generation ** gen)
+{
+    int * p = malloc(sizeof(float)*(*gen)->nbSubject);
+    int * p2 = malloc(sizeof(int)*(*gen)->nbSubject);
+    int r1,r2,i,j,k,l,m;
+    Generation * temp = newGenerationNull((*gen)->nbSubject);
+    float totalchoix = 0;
+    float totalFitness =0;
+    setNbGen(&temp, getNbGen(*gen)+1);
+    for(i =0;i<(*gen)->nbSubject;i++)
+    {
+        totalFitness += getFitness(getGenome(*gen, i));
+    }
+    printf("%f\n",totalFitness/((*gen)->nbSubject));
+    for(i =0;i<(*gen)->nbSubject;i++)
+    {
+        p[i] = ((getFitness(getGenome(*gen, i))/totalFitness)*100);
+        totalchoix += p[i];
+    }
+    for(j = 1;j<(*gen)->nbSubject;j++)
+    {
+        m = totalchoix;
+        r1 =rand()%100;
+        r2 =rand()%100;
+        for(i = 0; i<(*gen)->nbSubject;i++)
+        {
+            m -= p[i];
+            if(r1 >= m)
+                k = i;
+            if(r2 >= m)
+                l = i;
+        }
+        setGenome(&temp,j,crossover(getGenome(*gen, k), getGenome(*gen,l)));
+    }
+    setGenome(&temp, 0, getBest(*gen));
+    deleteGeneration((*gen));
+    *gen = temp;
+}
+void mutationGeneration(Generation ** gen)
+{
+    int i;
+    Genome * temp;
+    for(i = 0; i<(*gen)->nbSubject;i++)
+    {
+        temp = mutation(getGenome(*gen, i));
+        deleteGenome(getGenome(*gen, i));
+        setGenome(gen, i, temp);
+    }
+}
+Genome * getBest(const Generation * gen)
+{
+    int i,max;
+    Genome * pGenome;
+    max = 0;
+    pGenome = NULL;
+    for(i = 1; i< gen->nbSubject;i++)
+        if(getFitness(gen->tabGenomes[i]) > getFitness(gen->tabGenomes[max]))
+            max = i;    
+    pGenome = newGenomeNull(getNbHidden(getGenome(gen, max)));
+    for(i = 0; i< (getNbInput(getGenome(gen, max)) + getNbOutput(getGenome(gen, max)))*getNbHidden(getGenome(gen, max));i++)
+        setGene(&pGenome, i , getGene(getGenome(gen, max), i));
+    return pGenome;
 }
