b82224e7dd04d2a9c04bade3e1e2ab087f778d5e
p1402835 <Maganik@users.noreply.github.com>
2016-03-24 15:46:00 +0100

    makefile et chimpmunk

---
+++ autodidacte/src/Display_SDL.h
@@ -0,0 +1,29 @@
+#ifndef H_VG_DISPLAY_SDL
+#define H_VG_DISPLAY_SDL
+
+
+  /* Display_SDL.h    -----   LIF7 Auto-Didacte */
+
+/* structure Display_SDL */
+
+typedef struct {
+	Simulation * sim;
+	int dimx;
+	int dimy;
+} Display_SDL;
+
+
+/* functions Display_SDL */
+
+/* Constructors and destructor */
+Display_SDL * newDisplay_SDL(Simulation * sim, int x,int y);
+
+
+/* getters and setters*/
+
+
+/*general functions */
+
+
+
+#endif
