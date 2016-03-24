0347ab7fecadc582e56f3c9b2738cb14ff39d5c4
p1300301 <p1300301@localhost>
2016-03-24 17:48:27 +0100

    Correction d'une faute de frappe dans le main.

--- autodidacte/src/main.c
+++ autodidacte/src/main.c
@@ -48,7 +48,7 @@ int main(int argc, char const *argv[])
 
 	deleteNetwork(net);
 
-	deleteGenerati	on(gen1);
+	deleteGeneration(gen1);
 	regressionTestGenome();
 	regressionTestGeneration();
 	printf("\n\n\n");
