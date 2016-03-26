7287fad9d29e9ca4a7905fd76c1afef6250c7173
p1300301 <p1300301@localhost>
2016-03-26 23:06:26 +0100

    Modifications de Track.* par Florian

--- autodidacte/src/Track.c
+++ autodidacte/src/Track.c
@@ -2,44 +2,100 @@
 #error This source file is not C++ but rather C. Please use a C-compiler
 #endif
 
-
-	/*  Track.C   -----   LIF7 Auto-Didacte */
-
+        /*  Track.C   -----   LIF7 Auto-Didacte */
 
 #include "Track.h"
 #include "stdlib.h"
 #include "assert.h"
 #include "stdio.h"
+#include "Point.h"
 
+
+
+/* Track constructors */
 Track * newTrack (){
 	Track * nTrack;
 	nTrack = malloc (sizeof(Track));
 	assert (nTrack != NULL);
 	return nTrack;
+
+}
+void initTrack (Track * track , int IN , int OUT){
+	assert (track != NULL);
+	setnbPointIn (track , IN);
+	setnbPointOut (track , OUT);
+	track->trackIn = malloc(IN * sizeof(Point));
+	track->trackOut = malloc(OUT * sizeof(Point));
+	assert (track->trackIn != NULL);
+	assert (track->trackOut != NULL);
+
 }
 
-void initTrack (Track * Track){
-	int i;
-	printf ("Nombre de points ? :\n");
-	scanf ("%d",&(Track->nbPoints));
-	Track->tabPoints = malloc (Track->nbPoints*sizeof(Point));
-	for (i=0 ; i < Track->nbPoints ; i++){
-		printf ("Coordonnées point %d :\n",i);
-		scanf ("%f",&(Track->tabPoints[i].x));
-		scanf ("%f",&(Track->tabPoints[i].y));
-	}
+
+/*Track Get/Set */
+
+Point * getTrackOut (const Track * track , int position){
+	assert (track != NULL);
+	assert (position < getnbPointOut(track));
+	return track->trackOut[position];
+
 }
-void printTrack (Track * Track){
-	int i;
-	printf ("Nombre de points  : %d\n",Track->nbPoints);
-	for (i=0 ; i < Track->nbPoints ; i++){
-		printf ("Coordonnées point %d :\n",i);
-		printf ("%f\n",Track->tabPoints[i].x);
-		printf ("%f\n",Track->tabPoints[i].y);
+
+void setTrackOut (Track * track , int position , Point * newPoint){
+	assert (track != NULL);
+	assert (position < getnbPointOut(track));
+	track->trackOut[position] = newPoint;
+}
+
+int getnbPointIn (const Track * track){
+	assert (track != NULL);
+	return track->nbPointIn;
+}
+void setnbPointIn (Track * track , int nbPoints){
+	assert (track != NULL);
+	track->nbPointIn = nbPoints;
+}
+int getnbPointOut (const Track * track){
+	assert (track != NULL);
+	return track->nbPointOut;
+}
+void setnbPointOut (Track * track , int nbPoints){
+	assert (track != NULL);
+	track->nbPointOut = nbPoints;
+}
+
+Point * getTrackIn (const Track * track , int position){
+	assert (track != NULL);
+	assert (position < getnbPointIn(track));
+	return track->trackIn[position];
+}
+void setTrackIn (Track * track , int position , Point * newPoint){
+	assert (track != NULL);
+	assert (position < getnbPointIn(track));
+	track->trackIn[position] = newPoint;
+}
+
+
+
+/* Track functions */
+void printTrack (Track * track){
+	Point * pointTemp;
+	int counter;
+	assert (track != NULL);
+	for (counter = 0 ; counter < getnbPointIn(track) ; counter++){
+		pointTemp = getTrackIn(track , counter);
+		printf("Point intérieur n° %d : %f , %f\n",counter ,getX(pointTemp), getY(pointTemp));
+	}
+	for (counter = 0 ; counter < getnbPointOut(track) ; counter++){
+		pointTemp = getTrackOut(track , counter);
+		printf("Point extérieur n° %d : %f , %f\n",counter ,getX(pointTemp), getY(pointTemp));
 	}
 }
 
-void deleteTrack (Track * Track){
-	free (Track->tabPoints);
-	free (Track);
+
+/* Track destructor */
+void deleteTrack (Track * track){
+	free (track->trackIn);
+	free (track->trackOut);
+	free (track);
 }
