3f87f3604c1b787a568642e47c5c3642b91b5312
p1300301 <p1300301@localhost>
2016-05-10 11:06:35 +0200

    Simulation update avec doxy

--- autodidacte/src/Simulation.h
+++ autodidacte/src/Simulation.h
@@ -10,7 +10,7 @@
 #include "Generation.h"
 
 
-enum{ LEFT , RIGHT  , NONE };
+enum{ LEFT , RIGHT  ,  NONE };
 
 
 
@@ -30,9 +30,9 @@ typedef struct
     Car * car; /** Car adress */
     Point * initPosition; /** Starting position adress */
     float initOrientation; /** Initial car's orientation */
-    float nbTicks; /** Simulation progress */
     int fitness; /** Car's score */
     int sector; /** Current track's part where the car is */
+    int nbTicks; /** Number of simulation's tick done in the current sector */
 
 }Simulation;
 
@@ -46,9 +46,9 @@ typedef struct
  * @brief      Initialize a new simulation
  *
  * @param[in]  speed            New simulation speed
- * @param      genome           Genome adress to load on the car
- * @param      track            Track adress
- * @param      initPosition     Initial car's position adress
+ * @param[in,out]     genome           Genome adress to load on the car
+ * @param[in,out]     track            Track adress
+ * @param[in,out]     initPosition     Initial car's position adress
  * @param[in]  initOrientation	Initial car's orientation  
  *
  * @return     Simulation adress
@@ -65,7 +65,7 @@ Simulation * newSimulation(float speed, Genome * genome, Track * track, Point * 
 /**
  * @brief      Get the sim speed.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     The simulation's speed
  */
@@ -74,7 +74,7 @@ float getSimSpeed(Simulation * sim);
 /**
  * @brief      Set the sim speed.
  *
- * @param      sim       Simulation adress
+ * @param[in,out]      sim       Simulation adress
  * @param[in]  newSpeed  New simulation speed
  */
 void setSimSpeed(Simulation * sim, float newSpeed);
@@ -82,7 +82,7 @@ void setSimSpeed(Simulation * sim, float newSpeed);
 /**
  * @brief      Get the car.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     Car adress
  */
@@ -91,15 +91,15 @@ Car * getCar(Simulation * sim);
 /**
  * @brief      Set the car.
  *
- * @param      sim   Simulation adress
- * @param      car   New car adress
+ * @param[in,out]      sim   Simulation adress
+ * @param[in,out]      car   New car adress
  */
 void setCar(Simulation * sim, Car * car);
 
 /**
  * @brief      Get the init position.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     Intial position point adress
  */
@@ -108,15 +108,15 @@ Point * getInitPosition(Simulation * sim);
 /**
  * @brief      Set the init position.
  *
- * @param      sim         Simulation adress
- * @param      newInitPos  Initial position point adress
+ * @param[in,out]      sim         Simulation adress
+ * @param[in,out]     newInitPos  Initial position point adress
  */
 void setInitPosition(Simulation * sim, Point * newInitPos);
 
 /**
  * @brief      Get the init orientation.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     Current car's orientation
  */
@@ -125,7 +125,7 @@ float getInitOrientation(Simulation * sim);
 /**
  * @brief      Set the init orientation.
  *
- * @param      sim             Simulation adress
+ * @param[in,out]      sim             Simulation adress
  * @param[in]  newOrientation  New car's orientation
  */
 void setInitOrientation(Simulation * sim, float newOrientation);
@@ -133,7 +133,7 @@ void setInitOrientation(Simulation * sim, float newOrientation);
 /**
  * @brief      Get the nb ticks.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     Return the tick number
  */
@@ -142,7 +142,7 @@ int getNbTicks(Simulation * sim);
 /**
  * @brief      Set the nb ticks.
  *
- * @param      sim    Simulation adress
+ * @param[in,out]      sim    Simulation adress
  * @param[in]  newNb  new tick number
  */
 void setNbTicks(Simulation * sim, int newNb);
@@ -150,7 +150,7 @@ void setNbTicks(Simulation * sim, int newNb);
 /**
  * @brief      Get the track.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     Track adress
  */
@@ -159,8 +159,8 @@ Track * getTrack(Simulation * sim);
 /**
  * @brief      Set the track.
  *
- * @param      sim    Simulation adress
- * @param      track  Track adress
+ * @param[in,out]      sim    Simulation adress
+ * @param[in,out]      track  Track adress
  */
 void setTrack(Simulation * sim, Track * track);
 
@@ -168,7 +168,7 @@ void setTrack(Simulation * sim, Track * track);
 /**
  * @brief      Get the sim fitness.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     get the car score
  */
@@ -177,7 +177,7 @@ int getSimFitness(Simulation * sim);
 /**
  * @brief      Set the sim fitness.
  *
- * @param      sim         Simulation adress
+ * @param[in,out]      sim         Simulation adress
  * @param[in]  newFitness  New car score
  */
 void setSimFitness(Simulation * sim, int newFitness);
@@ -185,7 +185,7 @@ void setSimFitness(Simulation * sim, int newFitness);
 /**
  * @brief      Get the sector.
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return    Current sector 
  */
@@ -194,10 +194,29 @@ int getSector(Simulation * sim);
 /**
  * @brief      Set the sector.
  *
- * @param      sim        Simulation adress
+ * @param[in,out]      sim        Simulation adress
  * @param[in]  newSector  New sector
  */
 void setSector(Simulation * sim, int newSector);
+
+
+
+/**
+ * @brief      Get the number of ticks.
+ *
+ * @param[in,out]      sim   Simulation adress
+ *
+ * @return    Current number of ticks 
+ */
+int getNbTicks(Simulation * sim);
+
+/**
+ * @brief      Set the number of ticks.
+ *
+ * @param[in,out]      sim        Simulation adress
+ * @param[in]  newSector  New number of ticks
+ */
+void setNbTicks(Simulation * sim, int newSector);
 
 
 
@@ -209,14 +228,14 @@ void setSector(Simulation * sim, int newSector);
  * @brief      Give the new values to the sensors depending of the car position
  *             in the track
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  */
 void updateSensors(Simulation * sim);
 
 /**
  * @brief      Detect if the car change sector while moving 
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     0 if the checkpoint isn't crossed else 1
  */
@@ -225,7 +244,7 @@ int detectCheckPointCrossed(Simulation * sim);
 /**
  * @brief      Detect if the car collide with the track
  *
- * @param      sim     Simulation adress
+ * @param[in,out]      sim     Simulation adress
  * @param[in]  sector  Current sector
  *
  * @return     0 if there is no collide else 1
@@ -235,7 +254,7 @@ int detectCollision(Simulation * sim, int sector);
 /**
  * @brief      Allow the car to move once ,check all the conditions (checkpoints, collisions) and update the sensors 
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  *
  * @return     The car's fitness score
  */
@@ -250,7 +269,7 @@ int tickSimulation(Simulation * sim);
 /**
  * @brief      Delete a simulation
  *
- * @param      sim   Simulation adress
+ * @param[in,out]      sim   Simulation adress
  */
 void endSimulation(Simulation * sim);
 
