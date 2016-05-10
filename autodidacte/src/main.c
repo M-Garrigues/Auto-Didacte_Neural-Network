a5892ed23873d61b4dfcd4b14e54da1414e64935
p1402835 <p1402835@localhost>
2016-05-10 19:51:35 +0200

    modification pour que la simulation reprenne la ou le calcul c'est arret?

--- autodidacte/src/main.c
+++ autodidacte/src/main.c
@@ -33,7 +33,7 @@ int main()
 	initTrackFile(pTrack,f);
 	fclose(f);
 	pInit = intersectPoint (pTrack->trackIn[0] , pTrack->trackOut[1] , pTrack->trackOut[0] , pTrack->trackIn[1]);
-	displayManagement(pGen , pTrack, pInit, initOrient, 641, 540 , 200, "data/car.png");
+	displayManagement(pGen , pTrack, pInit, initOrient, 641, 540 , 60, "data/car.png");
 
 	deleteGeneration(pGen);
 	deleteTrack(pTrack);
