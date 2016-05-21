c018675ec654722338cbba45fb3c84b51985b09a
p1400915 <p1400915@localhost>
2016-05-21 17:26:02 +0200

    Test de regression Point , Car , Track

--- autodidacte/src/main.c
+++ autodidacte/src/main.c
@@ -20,7 +20,86 @@ void printNetwork(Network * net);
 void printLayer(Layer * layer);
 void testRegressionPOINT();
 */
+/*
+void unitTestPoint (){
+	Point *p1, *p2, *p3, *p4, *p5, *p6;
+ 	p1 = newPoint(-10,0);
+ 	p2 = newPoint(10,0);
+ 	p3 = newPoint(10,10);
+ 	p4 = newPoint(-10,-10);
+ 	assert (getX(p1)==-10);
+ 	assert (getY(p3)==10);
+ 	assert (intersect(p1,p2,p3,p4)==1);
+ 	p5 = intersectPoint(p1,p2,p3,p4);
+ 	assert (getX(p5)==0);
+ 	assert (getY(p5)==0);
+ 	setX(p4,20);
+ 	setY(p4,10);
+ 	setX(p1,20);
+ 	assert (intersect(p1,p2,p3,p4)==0);
+ 	p6 = middle (p2,p3);
+ 	assert (getX(p6)==10);
+ 	assert (getY(p6)==5);
+ 	assert (distance(p2,p6)==distance(p3,p6));
+ 	deletePoint(p1);
+  	deletePoint(p2);
+  	deletePoint(p3);
+  	deletePoint(p4);
+  	deletePoint(p5);
+  	deletePoint(p6);
+  	printf("Unit test for Point.* OK\n");
+}
 
+void unitTestCar (){
+	Car *c1;
+	Genome *g1;
+	Point *p1, *p2, *p3;
+	int tab[3] = {6, 5, 4};
+	p1 = newPoint (0,0);
+	g1 = newGenomeRandom(tab);
+	c1 = newCar();
+	initCar (c1, g1, p1, 0, 100, 50);
+	p2 = getFrontLeft(c1);
+	assert(getX(p2)==50);
+	assert(getY(p2)==-25);
+	p3 = getCenter(c1);
+	assert(getX(p3)==0);
+	assert(getY(p3)==0);
+	turnLeft(c1);
+	assert(getOrientation(c1)<0);
+	turnRight(c1);
+	assert(getOrientation(c1)==0);
+	deleteCar (c1);
+	printf("Unit test for Car.* OK\n");
+}
+
+void unitTestTrack (){
+	Track *t1, *t2;
+	Point *p0, *p1, *p2, *p3, *p4;
+	p0 = newPoint (0,0);
+	p1 = newPoint (10,50);
+	p3 = newPoint (10,10);
+	p4 = newPoint (50,50);
+	FILE * f = fopen("data/track.txt", "r");
+	t1 = newTrack();
+	initTrack(t1,2);
+	setTrackOut(t1,0,p1);
+	setTrackOut(t1,1,p0);
+	setTrackIn(t1,0,p3);
+	setTrackIn(t1,1,p4);
+	p2 = getTrackOut (t1,0);
+	assert(getX(p2)==getX(p1));
+	assert(getY(p2)==getY(p1));
+	assert(getNbPoints(t1)==2);
+	deleteTrack(t1);
+	t2 = newTrack();
+	initTrackFile (t2,f);
+	assert(getNbPoints(t2)!=0);
+	fclose(f);
+	deleteTrack(t2);
+	printf("Unit test for Track.* OK\n");
+
+}*/
 int main()
 {
 	FILE * f = fopen("data/track.txt", "r");
